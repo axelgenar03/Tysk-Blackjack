37b93a80bac49bd9e935f51ff90dcdd64c4008a2
Anton Norman <normananton03@gmail.com>
2023-07-23 16:07:45 +0200

    Deck Reset funktion

--- Deck.py
+++ Deck.py
@@ -11,4 +11,11 @@ class Deck:
                 deck.append(Card(val, face))
         self.deck = deck
 
-    
+    def reset(self):
+        deck = []
+        possible_faces = ["Hearts", "Diamonds", "Spades", "Clubs"]
+        possible_values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        for val in possible_values:
+            for face in possible_faces:
+                deck.append(Card(val, face))
+        self.deck = deck
