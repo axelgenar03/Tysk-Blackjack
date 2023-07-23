a8bab80ca4ea2573832e0e84caecff017480c7b9
Axel Genar <axel.genar@gmail.com>
2023-07-23 21:08:31 +0200

    added a function to replace a singe card from the users hand, works for now but will need overhaul later to work with multiple cards

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import random
 wallet = Wallet()
 deck_class = Deck()
 deck = deck_class.deck
+global player_hand
 
 
-
-def deal_hand():
-    user_hand = []
+def deal_hand():  
+    user_hand = []  
     for i in range(5):
         index = random.randint(0,len(deck))
         user_hand.append(deck[index])
@@ -17,19 +17,34 @@ def deal_hand():
     for card in user_hand:
         print(card.show())
     print("---------------")
+    return user_hand
+    
+    
 
 
 def dealer_hand():
-    user_hand = []
+    dealer_hand = []
     for i in range(5):
         index = random.randint(0,len(deck))
-        user_hand.append(deck[index])
+        dealer_hand.append(deck[index])
         del deck[index]
     #for card in user_hand:
+
+def change_hand(amogus):
+  
+    bad_card = int(input("What card do you want to exchange?"))
+    bad_card -= 1
+    index = random.randint(0,len(deck))
+    amogus[bad_card] = (deck[index])
+    del deck[index]    
+    print("\nYour hand: \n---------------")
+    for card in amogus:
+        print(card.show())
+    print("---------------")
         
 
-deal_hand()
-
+player_hand = deal_hand()
 dealer_hand()
 
+change_hand(player_hand)
 
