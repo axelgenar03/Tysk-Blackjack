35739e2c8e6a0c4fec9e125df437d97196f53776
Axel Genar <axel.genar@gmail.com>
2023-07-25 00:12:26 +0200

    gui amogus

---
+++ gui.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+window = Tk()
+canvas = Canvas()
+window.geometry("800x800")
+window.title("Blackjack")
+
+
+#    canvas.create_rectangle(10,10,110,110,outline ="black",fill ="white",width = 2)
+#  canvas.create_rectangle(210,10,310,210,outline ="black",fill ="white",width =2)
+
+
+
+
+canvas.pack()
+window.mainloop()
