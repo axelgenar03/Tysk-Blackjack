174e8ba825096a7e6b0e64aea9b985941da8b6f1
Axel Genar <axel.genar@gmail.com>
2023-07-25 13:11:22 +0200

    gui now shows hand

--- gui.py
+++ gui.py
@@ -6,17 +6,19 @@ class GUI:
         pass
 
     def gui(self,cards):
+        print(cards)
+        
         window = Tk()
         canvas = Canvas()
         window.geometry("800x800")
         window.title("Blackjack")
 
 
-        Label(window,pady=80,padx=40, text="Kort 1", bg="#FFFF00", fg="white").place(x=50, y=500)
-        Label(window,pady=80,padx=40, text="Kort 2", bg="#3300CC", fg="white").place(x=180, y=500)
-        Label(window,pady=80,padx=40, text="Kort 3", bg="#FF0099", fg="white").place(x=310, y=500)
-        Label(window,pady=80,padx=40, text="Kort 4", bg="#FF0123", fg="white").place(x=440, y=500)
-        Label(window,pady=80,padx=40, text="Kort 5", bg="#00FF00", fg="white").place(x=570, y=500)
+        Label(window, pady=80, padx=20, text=cards[0].show(), bg="#FFFFFF", fg="black", width=10, anchor='center').place(x=50, y=500)
+        Label(window, pady=80, padx=20, text=cards[1].show(), bg="#FFFFFF", fg="black", width=10, anchor='center').place(x=190, y=500)
+        Label(window, pady=80, padx=20, text=cards[2].show(), bg="#FFFFFF", fg="black", width=10, anchor='center').place(x=330, y=500)
+        Label(window, pady=80, padx=20, text=cards[3].show(), bg="#FFFFFF", fg="black", width=10, anchor='center').place(x=470, y=500)
+        Label(window, pady=80, padx=20, text=cards[4].show(), bg="#FFFFFF", fg="black", width=10, anchor='center').place(x=610, y=500)
 
 
 
